81febf55b6ccee1df11fca8e0bd8fe0ea8866210
Mohamed-Aidarus <maxamedmorgan3@gmail.com>
2024-06-19 14:07:44 +0300

    modified:   src/components/sections/dashboard/Home/Sales/TablePredict/Table.tsx

--- src/components/sections/dashboard/Home/Sales/TablePredict/Table.tsx
+++ src/components/sections/dashboard/Home/Sales/TablePredict/Table.tsx
@@ -20,55 +20,59 @@ import * as XLSX from 'xlsx';
 
 interface RowData {
   id: string;
-  department: string;
-  gpaS1: number;
-  gpaS2: number;
-  gpaS3: number;
-  gpaS4: number;
-  gpaS5: number;
-  gpaS6: number;
-  gpaS7: number;
-  gpaS8: number;
-  gender: string;
-  mode: string;
-  attS1: number;
-  attS2: number;
-  attS3: number;
-  attS4: number;
-  attS5: number;
-  attS6: number;
-  attS7: number;
-  attS8: number;
-  discounts: number;
-  noReExams: number;
-  prediction: string;
+  Department: string;
+  Sex: string;
+  Mode: string;
+  Att_S1: number;
+  Att_S2: number;
+  Att_S3: number;
+  Att_S4: number;
+  Att_S5: number;
+  Att_S6: number;
+  Att_S7: number;
+  Att_S8: number;
+
+  Schollarship: number;
+
+  GPA_S1: number;
+  GPA_S2: number;
+  GPA_S3: number;
+  GPA_S4: number;
+  GPA_S5: number;
+  GPA_S6: number;
+  GPA_S7: number;
+  GPA_S8: number;
+
+  
+  No_ReExam_Subjects: number;
+  Prediction: string;
 }
 
 const columns: GridColDef[] = [
-  { field: 'department', headerName: 'Department', flex: 1, minWidth: 250 },
-  { field: 'gender', headerName: 'Gender', flex: 0.5, minWidth: 90 },
-  { field: 'mode', headerName: 'Mode', flex: 0.5, minWidth: 90 },
-  { field: 'attS1', headerName: 'Attendance S1', type: 'number', flex: 0.5, minWidth: 150 },
-  { field: 'attS2', headerName: 'Attendance S2', type: 'number', flex: 0.5, minWidth: 150 },
-  { field: 'attS3', headerName: 'Attendance S3', type: 'number', flex: 0.5, minWidth: 150 },
-  { field: 'attS4', headerName: 'Attendance S4', type: 'number', flex: 0.5, minWidth: 150 },
-  { field: 'attS5', headerName: 'Attendance S5', type: 'number', flex: 0.5, minWidth: 150 },
-  { field: 'attS6', headerName: 'Attendance S6', type: 'number', flex: 0.5, minWidth: 150 },
-  { field: 'attS7', headerName: 'Attendance S7', type: 'number', flex: 0.5, minWidth: 150 },
-  { field: 'attS8', headerName: 'Attendance S8', type: 'number', flex: 0.5, minWidth: 150 },
-  { field: 'totalAttendance', headerName: 'Total Attendance', flex: 0.5, minWidth: 150 },
-  { field: 'discounts', headerName: 'Discounts', type: 'number', flex: 0.5, minWidth: 90 },
-  { field: 'noReExams', headerName: 'No. of Re-exams', type: 'number', flex: 0.5, minWidth: 150 },
-  { field: 'gpaS1', headerName: 'GPA S1', type: 'number', flex: 0.5, minWidth: 80 },
-  { field: 'gpaS2', headerName: 'GPA S2', type: 'number', flex: 0.5, minWidth: 80 },
-  { field: 'gpaS3', headerName: 'GPA S3', type: 'number', flex: 0.5, minWidth: 80 },
-  { field: 'gpaS4', headerName: 'GPA S4', type: 'number', flex: 0.5, minWidth: 80 },
-  { field: 'gpaS5', headerName: 'GPA S5', type: 'number', flex: 0.5, minWidth: 80 },
-  { field: 'gpaS6', headerName: 'GPA S6', type: 'number', flex: 0.5, minWidth: 80 },
-  { field: 'gpaS7', headerName: 'GPA S7', type: 'number', flex: 0.5, minWidth: 80 },
-  { field: 'gpaS8', headerName: 'GPA S8', type: 'number', flex: 0.5, minWidth: 80 },
-  { field: 'totalCGpa', headerName: 'CGPA', flex: 0.5, minWidth: 80 },
-  { field: 'prediction', headerName: 'Prediction', flex: 0.75, minWidth: 150 },
+  { field: 'Department', headerName: 'Department', flex: 1, minWidth: 250 },
+  { field: 'Sex', headerName: 'Sex', flex: 0.5, minWidth: 90 },
+  { field: 'Mode', headerName: 'Mode', flex: 0.5, minWidth: 90 },
+  { field: 'Att_S1', headerName: 'Att-S1', type: 'number', flex: 0.5, minWidth: 150 },
+  { field: 'Att_S2', headerName: 'Att-S2', type: 'number', flex: 0.5, minWidth: 150 },
+  { field: 'Att_S3', headerName: 'Att-S3', type: 'number', flex: 0.5, minWidth: 150 },
+  { field: 'Att_S4', headerName: 'Att-S4', type: 'number', flex: 0.5, minWidth: 150 },
+  { field: 'Att_S5', headerName: 'Att-S5', type: 'number', flex: 0.5, minWidth: 150 },
+  { field: 'Att_S6', headerName: 'Att-S6', type: 'number', flex: 0.5, minWidth: 150 },
+  { field: 'Att_S7', headerName: 'Att-S7', type: 'number', flex: 0.5, minWidth: 150 },
+  { field: 'Att_S8', headerName: 'Att-S8', type: 'number', flex: 0.5, minWidth: 150 },
+  { field: 'T_Att', headerName: 'T-Att', flex: 0.5, minWidth: 150 },
+  { field: 'Schollarship', headerName: 'Schollarship', type: 'number', flex: 0.5, minWidth: 90 },
+  { field: 'No_ReExam_Subjects', headerName: 'No-Re-exam', type: 'number', flex: 0.5, minWidth: 150 },
+  { field: 'GPA_S1', headerName: 'GPA S1', type: 'number', flex: 0.5, minWidth: 80 },
+  { field: 'GPA_S2', headerName: 'GPA S2', type: 'number', flex: 0.5, minWidth: 80 },
+  { field: 'GPA_S3', headerName: 'GPA S3', type: 'number', flex: 0.5, minWidth: 80 },
+  { field: 'GPA_S4', headerName: 'GPA S4', type: 'number', flex: 0.5, minWidth: 80 },
+  { field: 'GPA_S5', headerName: 'GPA S5', type: 'number', flex: 0.5, minWidth: 80 },
+  { field: 'GPA_S6', headerName: 'GPA S6', type: 'number', flex: 0.5, minWidth: 80 },
+  { field: 'GPA_S7', headerName: 'GPA S7', type: 'number', flex: 0.5, minWidth: 80 },
+  { field: 'GPA_S8', headerName: 'GPA S8', type: 'number', flex: 0.5, minWidth: 80 },
+  { field: 'CGPA', headerName: 'CGPA', flex: 0.5, minWidth: 80 },
+  { field: 'Prediction', headerName: 'Prediction', flex: 0.75, minWidth: 150 },
 ];
 
 const convertPercentageToGPA = (percentage) => {
@@ -107,34 +111,38 @@ const Table = (): ReactElement => {
 
       return {
         id: item._id,
-        department: item.Department,
-        gender: item.Sex,
-        mode: item.Mode,
-        attS1: item['Att-S1'],
-        attS2: item['Att-S2'],
-        attS3: item['Att-S3'],
-        attS4: item['Att-S4'],
-        attS5: item['Att-S5'],
-        attS6: item['Att-S6'],
-        attS7: item['Att-S7'],
-        attS8: item['Att-S8'],
-        totalAttendance: calculateAverage([
+        Department: item.Department,
+        Sex: item.Sex,
+        Mode: item.Mode,
+        Att_S1: item['Att-S1'],
+        Att_S2: item['Att-S2'],
+        Att_S3: item['Att-S3'],
+        Att_S4: item['Att-S4'],
+        Att_S5: item['Att-S5'],
+        Att_S6: item['Att-S6'],
+        Att_S7: item['Att-S7'],
+        Att_S8: item['Att-S8'],
+
+        T_Att: calculateAverage([
           item['Att-S1'], item['Att-S2'], item['Att-S3'], item['Att-S4'], item['Att-S5'],
           item['Att-S6'], item['Att-S7'], item['Att-S8']
         ]),
 
-        gpaS1: item['GPA-S1'],
-        gpaS2: item['GPA-S2'],
-        gpaS3: item['GPA-S3'],
-        gpaS4: item['GPA-S4'],
-        gpaS5: item['GPA-S5'],
-        gpaS6: item['GPA-S6'],
-        gpaS7: item['GPA-S7'],
-        gpaS8: item['GPA-S8'],
-        discounts: item.Discounts || 0,
-        noReExams: item['NO-Re-exams'] || 0,
-        totalCGpa: convertPercentageToGPA(totalCGpaPercentage),
-        prediction: item.Prediction,
+        Schollarship: item.Schollarship || 0,
+        No_ReExam_Subjects: item['NO-Re-exams'] || 0,
+
+        GPA_S1: item['GPA-S1'],
+        GPA_S2: item['GPA-S2'],
+        GPA_S3: item['GPA-S3'],
+        GPA_S4: item['GPA-S4'],
+        GPA_S5: item['GPA-S5'],
+        GPA_S6: item['GPA-S6'],
+        GPA_S7: item['GPA-S7'],
+        GPA_S8: item['GPA-S8'],
+
+
+        CGPA: convertPercentageToGPA(totalCGpaPercentage),
+        Prediction: item.Prediction,
       };
     });
   }, [data]);
